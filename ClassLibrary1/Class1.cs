7455a8c8fa9511f36fa95ffe0a7f5b991a37d9db
David Zhao (DEVDIV) <dazhao@microsoft.com>
2017-02-27 10:01:15 -0800

    init checkin

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public static class Class1
+    {
+        public static string GetString() => "Hello";
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public async Task<IActionResult> Contact()
+        {
+            using (var client = new HttpClient())
+            {
+                string result = await client.GetStringAsync("http://webapplication2/api/values");
+
+                ViewData["Message"] = result;
+            }
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
